a398f44df8df2aaac417d5aa7b1e12650533be0d
Farnese <153958765+farneses@users.noreply.github.com>
2024-01-15 18:16:10 -0300

    added nyxx

--- bin/latria.dart
+++ bin/latria.dart
@@ -1,3 +1,20 @@
-void main(List<String> arguments) {
-  print('Hello world!');
+import 'package:nyxx/nyxx.dart';
+
+void main() async {
+  final client = await Nyxx.connectGateway(
+    'TOKEN',
+    GatewayIntents.allUnprivileged,
+    options: GatewayClientOptions(plugins: [logging, cliIntegration]),
+  );
+
+  final botUser = await client.users.fetchCurrentUser();
+
+  client.onMessageCreate.listen((event) async {
+    if (event.mentions.contains(botUser)) {
+      await event.message.channel.sendMessage(MessageBuilder(
+        content: 'Hi There!',
+        replyId: event.message.id,
+      ));
+    }
+  });
 }
